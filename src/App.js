a00704c3486c641024a1b5857ded23b47798141b
Damian Amalraj <45610772+damianamalraj@users.noreply.github.com>
2023-02-27 13:12:42 +0100

    Update page title

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import Clock from "./components/clock/Clock";
-import Quote from "./components/quote/index";
-import Weather from "./components/weather/Weather";
-import News from "./components/news/index";
-import Social from "./components/social/index";
+import React from 'react';
+import Clock from './components/clock/Clock';
+import Quote from './components/quote/index';
+import Weather from './components/weather/Weather';
+import News from './components/news/index';
+import Social from './components/social/index';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="header-container">
         <div className="header">
-          <h1>IT-Högskolan</h1>
+          <h1>PWA Dashboard</h1>
         </div>
       </div>
       <div className="wgt">
